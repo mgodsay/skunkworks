a002c6711ce31f52115c11a9a5f0868096ed257a
Francis Xavier, Triny <tfrancisxavier@ebay.com>
2016-06-21 12:56:29 -0700

    Merged on boarding changes

--- bot/app/app.js
+++ bot/app/app.js
@@ -15,155 +15,90 @@
 // [START app]
 //'use strict';
 
-var startChattingFlag = false;
-var genderFlag = false;
-var  ageFlag = false;
+var dbEndPoint = "http://localhost:3000",
+    token = "EAABs0h5eyPABAHqwxF03wYggEcUOpHs2XnKNEuZCU0gt6SX2QoBCjPBcwZA12ar47bcrZADYodD8h3wdSiQ0lTVFEhg3vZCC8CgukDbEyqTIAMH6htyq6NS8BELaIXSVid1lLCvOAk4OmZAb1Y7M0D3kzm9I3UemjIbvzNr8hbwZDZD";
+    //token = "CAAT6h9nizVIBAJ5o0GqEH52Wlwf8Anc8JJAzqmzJgH0ZAZAPZBTvCleKbGcrTR4fOlNRK12JiHHld2GjgT8seSkeXKveadeMEqHS6KcKYSfghHo2ux5h0doqc9WZA3WoeCgZA5QJCJWUZA8UIeh0nUpPKEOeyRXmndVm5MnmD8SqlI9YUwFlDjuEtK84fOFGSL76xI6MuhrAZDZD";
+
+var startChattingFlag = false,
+  genderFlag = false,
+  ageFlag = false;
+
+var express = require('express'),
+    request = require("request"),
+    app = express(),
+    port = 8080,
+    https = require('https'),
+    http = require('http'),
+    fs = require('fs'),
+    bodyParser = require('body-parser');
+
 console.log("startChattingFlag is :",startChattingFlag);
 console.log("genderFlag is :",genderFlag);
 console.log("ageFlag is :",ageFlag);
-var express = require('express');
-var request = require("request")
-var app = express();
-var port = 8080;
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-var bodyParser = require('body-parser');
-app.use(bodyParser.json()); // support json encoded bodies
-app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get('/', function(req, res) {
   res.status(200).send('Hello, world!');
 });
 
+
 app.get('/webhook/', function (req, res) {
-  console.log("In the get");
+  //console.log("In the get");
   if (req.query['hub.verify_token'] === 'mobileFeatureQE') {
     res.send(req.query['hub.challenge']);
   }
-  res.send('Error, wrong validation token');
+  res.send('Error, invalid validation token');
 })
 
 
 app.post('/webhook/', function (req, res) {
-  console.log("I am in the webhook");
-  console.log(req.body);
+  //console.log("I am in the webhook " + req.body);
   messaging_events = req.body.entry[0].messaging;
   for (i = 0; i < messaging_events.length; i++) {
-   event = req.body.entry[0].messaging[i];
-   sender = event.sender.id;
-   if (event.message && event.message.text) {
-    text = event.message.text;
-    if (text === 'Deals') {
-      sendGenericMessage(sender);
-      continue;
-    }else if (text === 'iPhone6'){
-      sendGenericMessage(sender);
-      continue;
-    }else if(text === 'Xbox 360'){
-      sendGenericMessage(sender);
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      console.log("text--->" + text);
+      if (text === 'Deals') {
+        console.log('Inside deals');
+        getProducts(sender, 'iPhone');
+        continue;
+      }else{
+        // Handle a text message from this sender
+        callFacebookAPI(sender, "Text received, echo: "+ text.substring(0, 200));
+      }
+    }
+    if (event.postback) {
+      text = JSON.stringify(event.postback);
+      respondToPostbacks(sender, event.postback);
       continue;
     }
-    else{
-      // Handle a text message from this sender
-      sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
-    }
   }
-  if (event.postback) {
-    text = JSON.stringify(event.postback);
-   // sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token);
-   respondToPostbacks(sender, event.postback);
-    continue;
-  }
-}
-res.sendStatus(200);
+  res.sendStatus(200);
 });
-
-var token = "CAAT6h9nizVIBAJ5o0GqEH52Wlwf8Anc8JJAzqmzJgH0ZAZAPZBTvCleKbGcrTR4fOlNRK12JiHHld2GjgT8seSkeXKveadeMEqHS6KcKYSfghHo2ux5h0doqc9WZA3WoeCgZA5QJCJWUZA8UIeh0nUpPKEOeyRXmndVm5MnmD8SqlI9YUwFlDjuEtK84fOFGSL76xI6MuhrAZDZD";
-//Commenting the local token
-//var token = "EAAHBkfH5Ty4BAFOwopgrcBJiqjgWIbAzraZALZCwH5V2UGaK8mEzS2TKIsLX3rNIKWBTjoVBLPgURNQa7fATjF4hr8OMlNksCkXh24cVcbZBA3XyIw9JyqSaD3DyblCet9uYRbQg3437JXZAKqc8NEjh9NkEULdL3fpOPdhw6gZDZD";
-function sendTextMessage(sender, text) {
-  console.log("In the sendTextMessage call");
-  messageData = {
-    text:text
-  }
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
-}
 
 //Respond to Postbacks
 function respondToPostbacks(sender,text){
-  console.log("Responding to postback");
-  console.log(text);
-  // postback = JSON.parse(text);
-
- 
-  cases = text.payload.split(":");
-  console.log(cases[0]);
-  messageData = "";
-  if(cases[0] === "Details"){
-      messageData = {
-    "attachment": {
-      "type": "template",
-      "payload": {
-        "template_type": "generic",
-        "elements": [{
-          "title": "Apple iPhone 6 Plus a1522 16GB for AT&T Gold Silver or Gray",
-          "subtitle": "Daily Deals",
-          "image_url": "http://i.ebayimg.com/00/s/MTAwMFgxMDAw/z/5tMAAOSw9mFWGNgN/$_1.JPG?set_id=880000500F",
-          "buttons": [{
-            "type": "postback",
-            "payload":  "Images: 252062798827 ",
-            "title": "More Images"
-          }, {
-            "type": "postback",
-            "title": "Buy It Now",
-            "payload": "BIN: 252062798827"
-          },
-          {
-            "type": "postback",
-            "title": "Deals",
-            "payload": "Deals"
-          }
-          ]
-        },{
-          "title": "Item Details",
-          "subtitle": "Seller Refurbished, Apple 16GB iPhone 6 Plus",
-          "image_url": "http://i.ebayimg.com/00/s/MTAwMFgxMDAw/z/EygAAOSw14xWGNgN/$_1.JPG?set_id=880000500F",
-          "buttons": [{
-            "type": "postback",
-            "payload":  "Deals",
-            "title": "Deals"
-          }, {
-            "type": "postback",
-            "title": "Buy It Now",
-            "payload": "BIN: 151876435362"
-          }]
-        }]
-      }
-    }
-  };
-  
-
-  }
-  if(cases[0] === "BIN"){
-
-  }
-  
+  console.log(text.payload);
+  var cases = text.payload.split(":");
   if((startChattingFlag === false)&&(cases[0] === "startChatting")){
+    onBoarding(text);
+  }else{
+    var action = text.payload.split(":"),
+        callback = action[0] + "(" + sender + ",'" + action[1] + "')";
+    eval(callback);
+  }
+}
+
+
+function onBoarding(text){
+  var cases = text.payload.split(":");
+   if((startChattingFlag === false)&&(cases[0] === "startChatting")){
     startChattingFlag = true;
     console.log("startChattingFlag in startChatting flow :",startChattingFlag);
     messageData = {
@@ -172,7 +107,7 @@ function respondToPostbacks(sender,text){
       "payload": {
         "template_type": "generic",
         "elements": [{
-          "title": "Great!let us get to know you better",
+          "title": "Great! Let us get to know you better",
           "buttons": [{
             "type": "postback",
             "payload":"Female",
@@ -192,6 +127,7 @@ function respondToPostbacks(sender,text){
       }
     }
   };
+
   console.log("Before Sending Request");
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
@@ -299,67 +235,118 @@ function respondToPostbacks(sender,text){
 
 
   }
-
 }
 
-//To send Structured Data
+//SRP
+var getProducts = function(sender, keyword){
+  restClient().get(dbEndPoint + "/products?q="+keyword, function (data, response) {
+    showSearchResults(sender, data.body);
+  });
+}
 
-function sendGenericMessage(sender) {
-  messageData = {
+var showSearchResults = function(sender, responseData) {
+  var elements = [];
+  responseData.forEach(function(object){
+    var tempData = {};
+    tempData.title = object.title;
+    tempData.subtitle = object.subtitle;
+    tempData.image_url = object.image_url;
+    tempData.buttons = [{"type": "postback", "payload" : "getItemDetails:" + object.id, "title":"View Details"},
+                        {"type": "postback", "payload" : "buyItem: " + object.id, "title":"Buy It Now"}];
+    elements.push(tempData);
+  });
+
+  callFacebookAPI(sender, elements);
+};
+
+//View item
+var getItemDetails = function(sender, itemId){
+  restClient().get(dbEndPoint + "/products/"+itemId, function (data, response) {
+    showItemDetails(sender, data.body);
+  });
+}
+
+var showItemDetails = function(sender, object) {
+  var elements = [],
+      tempData = {};
+  tempData.title = object.title;
+  tempData.subtitle = object.subtitle;
+  tempData.image_url = object.image_url;
+  tempData.buttons = [{"type": "postback", "payload" : "loadImages:" + object.id , "title":"More Images"},
+                      {"type": "postback", "payload" : "buyItem:" + object.id, "title":"Buy It Now"},
+                      {"type": "postback", "payload" : "getProducts:iPhone", "title":"< Back to search results"}];
+  elements.push(tempData);
+  callFacebookAPI(sender, elements);
+};
+
+
+//More Images
+var loadImages = function(sender, itemId){
+  restClient().get(dbEndPoint + "/images/"+itemId, function (data, response) {
+    showMoreImages(sender, data.body);
+  });
+}
+
+var showMoreImages = function(sender, responseData) {
+  var elements = [];
+  responseData.urls.forEach(function(object){
+    var tempData = {};
+    tempData.title = responseData.title;
+    tempData.image_url = object;
+    tempData.buttons = [{"type": "postback", "payload" : "buyItem:" + responseData.id, "title":"Buy It Now"},
+                      {"type": "postback", "payload" : "getItemDetails:" + responseData.id, "title":"< Back to view item"}];
+    elements.push(tempData);
+  });
+
+  callFacebookAPI(sender, elements);
+ };
+
+//buyItem
+var buyItem = function(sender, itemId){
+  callFacebookAPI(sender, "Thank you! Your order has been placed successfully. Your item will be delivered on or before 27th June 2016.");
+};
+
+function callFacebookAPI(sender, elements){
+  var messageData = (typeof elements == 'string') ? {text:elements} : {
     "attachment": {
       "type": "template",
       "payload": {
         "template_type": "generic",
-        "elements": [{
-          "title": "Apple iPhone 6 Plus a1522 16GB for AT&T Gold Silver or Gray",
-          "subtitle": "Daily Deals",
-          "image_url": "http://i.ebayimg.com/00/s/MTAwMFgxMDAw/z/5tMAAOSw9mFWGNgN/$_35.JPG",
-          "buttons": [{
-            "type": "postback",
-            "payload":  "Details: 252062798827 ",
-            "title": "View Details"
-          }, {
-            "type": "postback",
-            "title": "Buy It Now",
-            "payload": "BIN: 252062798827"
-          }]
-        },{
-          "title": "LG Nexus 5X H790 32GB (Factory GSM Unlocked) 4G LTE Android Smartphone- US Model",
-          "subtitle": "Daily Deals",
-          "image_url": "http://i.ebayimg.com/00/s/NTAwWDUwMA==/z/ToMAAOSw3YNXYzV6/$_35.JPG",
-          "buttons": [{
-            "type": "postback",
-            "title": "View Details",
-            "payload":  "Details: 151876435362 ",
-          }, {
-            "type": "postback",
-            "title": "Buy It Now",
-            "payload": "BIN: 151876435362"
-          }]
-        }]
+        "elements": elements
       }
     }
   };
+
+  //console.log("messageData ---> " + messageData);
   request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token:token},
-    method: 'POST',
-    json: {
-      recipient: {id:sender},
-      message: messageData,
+      url: 'https://graph.facebook.com/v2.6/me/messages',
+      qs: {access_token:token},
+      method: 'POST',
+      json: {
+        recipient: {id:sender},
+        message: messageData,
+      }
+    }, function(error, response, body) {
+      if (error) {
+        console.log('Error sending message: ', error);
+      } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+      }
     }
-  }, function(error, response, body) {
-    if (error) {
-      console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-      console.log('Error: ', response.body.error);
-    }
-  });
+  );
+
 }
+
 // Start the server
 var server = app.listen(process.env.PORT || '8080', '0.0.0.0', function() {
   console.log('App listening at http://%s:%s', server.address().address,
     server.address().port);
   console.log('Press Ctrl+C to quit.');
 });
+
+var restClient = function(){
+  return(new (require('node-rest-client').Client)());
+}
+
 // [END app]
+
